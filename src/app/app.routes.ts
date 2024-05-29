b343ad99e95232086e05f23fd7292b3eb4657cbf
FakirHerif <basdemir35@hotmail.com.tr>
2024-05-29 04:23:46 +0300

    translate pipe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomePageComponent } from './routes/home-page/home-page.component';
 import { authRoutes } from './routes/auth/auth.routes';
 import { productsRoutes } from './routes/products/products.routes';
 import { categoriesRoutes } from './routes/categories/categories.routes';
+import { TranslateComponent } from './shared/components/translate/translate.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePageComponent 
+    },
+    {
+        path: 'translate',
+        component: TranslateComponent 
     },
 
     ...authRoutes,
