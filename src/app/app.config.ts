e746567d7e156de043526a16f4a34511e34addc3
FakirHerif <basdemir35@hotmail.com.tr>
2024-06-01 05:14:30 +0300

    loading interceptor and service for other requests other than get

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, EnvironmentProviders, Provider } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { loadingInterceptor } from './shared/interceptors/loading.interceptor';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/');
@@ -24,7 +24,7 @@ translateModuleProviders = TranslateModule.forRoot({
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(),
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withInterceptors([loadingInterceptor])),
     ...(translateModuleProviders || [])
   ]
 };
